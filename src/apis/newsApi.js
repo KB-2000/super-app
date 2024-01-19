5bd18f2069737abd06bfd07f60fd736eb70a8242
KB-2000 <kaushikbansde09@gmail.com>
2024-01-19 16:27:51 +0530

    api

--- src/apis/newsApi.js
+++ src/apis/newsApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 export const getnewsDetails = async () => {
     try {
+        const ap=0;
         const reqUrl = `https://newsapi.org/v2/everything?q=Apple&apiKey=a624f2b674c44e37b5b07aaba51f16a9`  
         const response = await axios.get(reqUrl)
         console.log(response.data.articles[1])
--- src/apis/weatherApi.js
+++ src/apis/weatherApi.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 export const getWeatherDetails = async () => {
     try {
+        const api = 0;
         const reqUrl = "http://api.weatherapi.com/v1/current.json?key=c3ec091e36f64ca997964016241701&q=latur"
         const response = await axios.get(reqUrl);
         console.log( response.data.location.localtime)
