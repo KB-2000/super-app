a9a65749b1b5e9823824c711bf6413f94ca11738
KB-2000 <kaushikbansde09@gmail.com>
2024-01-22 17:10:30 +0530

    http to https

--- src/apis/moviesApi.js
+++ src/apis/moviesApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getMovieDetails = async (genre) => {
     const apiKey = process.env.REACT_APP_MOVIE_API_KEY 
-    const reqUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${genre}`
+    const reqUrl = `https://www.omdbapi.com/?apikey=${apiKey}&s=${genre}`
       try {
           const response = await axios.request(reqUrl);
           return response.data.Search
--- src/apis/weatherApi.js
+++ src/apis/weatherApi.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const getWeatherDetails = async () => {
     try {
-        const reqUrl = `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=latur`
+        const reqUrl = `https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=latur`
         const response = await axios.get(reqUrl);
         console.log(process.env.REACT_APP_WEATHER_API_KEY)
         return response;
